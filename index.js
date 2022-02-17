a1bc155ddb470b9f6afcb8782316a269df04418d
Pankorn Chantarod <pankorn.ch@gmail.com>
2022-02-17 23:17:57 +0700

    :tada: init

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler();
